11f25539f9361c5f5d628249bb782d7483c6ec7a
Ganesh0265 <ganeshpariyar265@gmail.com>
2025-06-04 23:58:43 +0545

    completed setup for django

--- wardrobe_manager/api/views.py
+++ wardrobe_manager/api/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def Main(request):
+def main(request):
     return HttpResponse("This is a simple http response")
